bf9bac51c507720bd2060a7bf1ef7d072ac18e67
osdodo <dodomatrix@pm.me>
2025-06-12 11:09:26 +0800

    add nextjs-pages-router example

---
+++ examples/nextjs-pages-router/src/pages/api/[...unify].ts
@@ -0,0 +1,27 @@
+import { createSource } from "unify-api";
+import { SolanaPlugin, EVMPlugin } from "@unify-api/plugins/web3";
+import { Hono } from "hono";
+import { handle } from "@hono/node-server/vercel";
+import type { PageConfig } from "next";
+
+export const config: PageConfig = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const app = new Hono().basePath("/api");
+
+const source = createSource({
+  app,
+});
+
+source.register([EVMPlugin, SolanaPlugin]);
+
+app.get("/hello", (c) => {
+  return c.json({
+    message: "Hello from Hono!",
+  });
+});
+
+export default handle(app);
---
+++ examples/nextjs-pages-router/src/pages/index.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { createClient } from "@unify-api/client";
+import { EVMPlugin, SolanaPlugin } from "@unify-api/plugins/web3";
+
+export default function Home() {
+  const [evmBalanceData, setEvmBalanceData] = useState<any>(null);
+  const [solanaBalanceData, setSolanaBalanceData] = useState<any>(null);
+
+  useEffect(() => {
+    const evmClient = createClient(EVMPlugin, {
+      baseURL: "http://localhost:3000/api",
+    });
+
+    const solanaClient = createClient(SolanaPlugin, {
+      baseURL: "http://localhost:3000/api",
+    });
+
+    const fetchEvmBalance = async () => {
+      try {
+        const res = await evmClient.balance.findOne({
+          where: {
+            address: "0x4f00D43b5aF0a0aAd62E9075D1bFa86a89CDb9aB",
+            network: "iotex",
+          },
+        });
+        if (res.data) {
+          setEvmBalanceData(res.data);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    const fetchSolanaBalance = async () => {
+      try {
+        const res = await solanaClient.balance.findOne({
+          where: {
+            address: "11111111111111111111111111111112",
+          },
+        });
+        if (res.data) {
+          setSolanaBalanceData(res.data);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchEvmBalance();
+    fetchSolanaBalance();
+  }, []);
+
+  return (
+    <div className="min-h-screen w-full flex flex-col items-center p-10">
+      {evmBalanceData && <p>{JSON.stringify(evmBalanceData, null, 2)}</p>}
+      {solanaBalanceData && <p>{JSON.stringify(solanaBalanceData, null, 2)}</p>}
+    </div>
+  );
+}
