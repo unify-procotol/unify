7ad7e8e80508e0d0b5259bb2948e1612ea1dc02b
osdodo <dodomatrix@pm.me>
2025-06-24 12:33:42 +0800

    update global example

--- examples/global/adapters/entity.ts
+++ examples/global/adapters/entity.ts
@@ -10,6 +10,7 @@ import { Entity } from "../entities/entity";
 
 const entities: Entity[] = [
   {
+    name: "PostEntity",
     ast: {
       kind: "ClassDeclaration",
       name: "PostEntity",
@@ -69,6 +70,7 @@ const entities: Entity[] = [
     },
   },
   {
+    name: "UserEntity",
     ast: {
       kind: "ClassDeclaration",
       name: "UserEntity",
@@ -130,34 +132,59 @@ const entities: Entity[] = [
 ];
 
 export class EntityAdapter implements DataSourceAdapter<Entity> {
-  async findMany(args: FindManyArgs<Entity>): Promise<Entity[]> {
+  async findMany(args?: FindManyArgs<Entity>): Promise<Entity[]> {
+    const where = args?.where || {};
+    if (where.name) {
+      return entities.filter((entity) => entity.name === where.name);
+    }
     return entities;
   }
 
   async findOne(args: FindOneArgs<Entity>): Promise<Entity | null> {
+    const where = args.where;
+    if (where.name) {
+      return entities.find((entity) => entity.name === where.name) || null;
+    }
     return null;
   }
 
   async create(args: CreationArgs<Entity>): Promise<Entity> {
+    const { name, ast } = args.data;
+    if (!name || !ast) {
+      throw {
+        status: 400,
+        message: "name and ast are required",
+      };
+    }
+
+    entities.push({
+      name,
+      ast,
+    });
+
     return {
-      ast: {
-        kind: "ClassDeclaration",
-        name: "",
-        modifiers: [],
-        members: [],
-      },
+      name,
+      ast,
     };
   }
 
   async update(args: UpdateArgs<Entity>): Promise<Entity> {
-    return {
-      ast: {
-        kind: "ClassDeclaration",
-        name: "",
-        modifiers: [],
-        members: [],
-      },
-    };
+    const { name, ast } = args.data;
+    if (!name || !ast) {
+      throw {
+        status: 400,
+        message: "name and ast are required",
+      };
+    }
+    const entity = entities.find((entity) => entity.name === name);
+    if (!entity) {
+      throw {
+        status: 404,
+        message: "entity not found",
+      };
+    }
+    entity.ast = ast;
+    return entity;
   }
 
   async delete(args: DeletionArgs<Entity>): Promise<boolean> {
--- examples/global/entities/entity.ts
+++ examples/global/entities/entity.ts
@@ -1,6 +1,7 @@
 import { ClassDeclaration, InterfaceDeclaration } from "../utils/ast-converter";
 
 export class Entity {
+  name: string = "Entity";
   ast: ClassDeclaration | InterfaceDeclaration = {
     kind: "ClassDeclaration",
     name: "Entity",
