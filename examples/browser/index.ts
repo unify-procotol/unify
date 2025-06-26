5bd7a0cdebbac3866dd47bcf2a3c384b877fce53
osdodo <dodomatrix@pm.me>
2025-06-26 09:16:03 +0800

    update browser example

--- examples/browser/index.ts
+++ examples/browser/index.ts
@@ -6,7 +6,7 @@ import { PostAdapter } from "./adapters/post";
 import { WalletPlugin } from "@unilab/uniweb3";
 import { WalletEntity } from "@unilab/uniweb3/entities";
 import { Plugin } from "@unilab/core";
-import { Logging } from "@unilab/core/middleware";
+import { createHookMiddleware, Logging } from "@unilab/core/middleware";
 
 const MyPlugin: Plugin = {
   entities: [UserEntity, PostEntity],
@@ -16,10 +16,86 @@ const MyPlugin: Plugin = {
   ],
 };
 
+const HookMiddleware = createHookMiddleware((hookManager) => {
+  hookManager
+    .beforeCreate(async (args, _, context) => {
+      console.log(
+        "🚀 Builder: Before Create Hook",
+        "args: ",
+        args,
+        "context: ",
+        context
+      );
+    })
+    .afterCreate(async (args, result, context) => {
+      console.log(
+        "✨ Builder: After Create Hook",
+        "result: ",
+        result,
+        "context: ",
+        context
+      );
+    })
+    .beforeUpdate(async (args, _, context) => {
+      console.log(
+        "🔄 Builder: Before Update Hook",
+        "args: ",
+        args,
+        "context: ",
+        context
+      );
+    })
+    .afterUpdate(async (args, result, context) => {
+      console.log(
+        "✅ Builder: After Update Hook",
+        "result: ",
+        result,
+        "context: ",
+        context
+      );
+    })
+    .beforeDelete(async (args, _, context) => {
+      console.log(
+        "🗑️ Builder: Before Delete Hook",
+        "args: ",
+        args,
+        "context: ",
+        context
+      );
+    })
+    .afterDelete(async (args, result, context) => {
+      console.log(
+        "💀 Builder: After Delete Hook",
+        "result: ",
+        result,
+        "context: ",
+        context
+      );
+    })
+    .beforeAny(async (args, _, context) => {
+      console.log(
+        "🔄 Builder: Before Any Hook",
+        "args: ",
+        args,
+        "context: ",
+        context
+      );
+    })
+    .afterAny(async (args, result, context) => {
+      console.log(
+        "✅ Builder: After Any Hook",
+        "result: ",
+        result,
+        "context: ",
+        context
+      );
+    });
+});
+
 UnifyClient.init({
   enableDebug: true,
   plugins: [MyPlugin, WalletPlugin],
-  middleware: [Logging()],
+  middleware: [HookMiddleware, Logging()],
 });
 
 // const fetchUser = async () => {
