ce0988d18b481d00df85ffa97a98e0fe69dbed33
osdodo <dodomatrix@pm.me>
2025-06-27 17:55:01 +0800

    update entity-getter demo

--- examples/entity-getter/index.ts
+++ examples/entity-getter/index.ts
@@ -1,8 +1,8 @@
 import { UserEntity } from "./entities/user";
-import { entity } from "@unilab/unify";
+import { clearAllEntityPools, entity } from "@unilab/unify";
 
 function test() {
-  const obj = {
+  const obj: any = {
     id: "1",
     name: "test",
     email: "test@test.com",
@@ -12,6 +12,11 @@ function test() {
   entity(UserEntity).Get(obj).click("test2");
 
   console.log("name:", entity(UserEntity).Get(obj).name);
+
+  // 回收obj时，对应的entity instances条目会被自动清理
 }
 
 test();
+
+// 清空 entity class pool
+clearAllEntityPools();
