333cf8a8fc588a0e1823ccad75b27457adab7bbb
DLHTX <35157091+DLHTX@users.noreply.github.com>
2025-07-10 09:34:30 +0800

    fix: update version of @unilab/urpc-adapters to 0.0.3

---
+++ packages/urpc-cli/myprojet/entities/post.ts
@@ -0,0 +1,21 @@
+import { Fields } from "@unilab/urpc-core";
+import type { UserEntity } from "./user";
+
+export class PostEntity {
+  @Fields.string()
+  id = "";
+
+  @Fields.string()
+  title = "";
+
+  @Fields.string()
+  content = "";
+
+  @Fields.string()
+  userId = "";
+
+  @Fields.record(() => require("./user").UserEntity, {
+    optional: true,
+  })
+  user?: UserEntity;
+}
---
+++ packages/urpc-cli/myprojet/entities/user.ts
@@ -0,0 +1,21 @@
+import { Fields } from "@unilab/urpc-core";
+import { PostEntity } from "./post";
+
+export class UserEntity {
+  @Fields.string()
+  id = "";
+
+  @Fields.string()
+  name = "";
+
+  @Fields.string()
+  email = "";
+
+  @Fields.string()
+  avatar = "";
+
+  @Fields.array(() => PostEntity, {
+    optional: true,
+  })
+  posts?: PostEntity[];
+}
